1d328e4158a079175b9033c021bc2028373c703c
이운린 <105215297+likegitman@users.noreply.github.com>
2024-07-02 23:47:00 +0900

    chore: Employees controller prisma로 타입 설정

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,3 +1,4 @@
+
 import {
   Controller,
   Get,
@@ -6,23 +7,23 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
-import { CreateEmployeeDto } from './dto/create-employee.dto';
-import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import { Prisma } from '@prisma/client';
 
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
 
   @Post()
-  create(@Body() createEmployeeDto: CreateEmployeeDto) {
+  create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
     return this.employeesService.create(createEmployeeDto);
   }
 
   @Get()
-  findAll() {
-    return this.employeesService.findAll();
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return this.employeesService.findAll(role);
   }
 
   @Get(':id')
@@ -33,7 +34,7 @@ export class EmployeesController {
   @Patch(':id')
   update(
     @Param('id') id: string,
-    @Body() updateEmployeeDto: UpdateEmployeeDto,
+    @Body() updateEmployeeDto: Prisma.EmployeeUpdateInput,
   ) {
     return this.employeesService.update(+id, updateEmployeeDto);
   }
